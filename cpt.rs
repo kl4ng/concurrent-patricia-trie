a814f2d4e6c7524547ad1f5e7b3c99dec4018d52
Kevin Lang <klang2012@gmail.com>
2015-03-21 22:52:29 -0400

    fleshed out data types and commented them, may have to change them around using ref and &, etc., later on, due to strictness of Rust

--- cpt.rs
+++ cpt.rs
@@ -7,36 +7,58 @@
 // by Niloufar Shafiei
 
 use std::sync::Arc;
-use self::Node::*;
 
+// The CPT simply is a root to the tree
 pub struct CPT {
-    root: Arc<Node>
+    root: Arc<Internal>
 }
 
 enum Node {
-    Leaf { 
-        label:  String,
-        info:   Info,
-    },
-
-    Internal {
-        label:  String,
-    }
+    Leaf(Leaf),
+    Internal(Internal),
 }
 
+// In order for Leaf to be both 'type of Node' and a distinct type
+// we need to have the actual fields stored in its own struct
+// Thus, we have enum value Node::Leaf and the distinct type Leaf
+struct Leaf {
+    label:  String,
+    info:   Info,
+}
+
+// See Leaf struct for details as to why we need a struct and
+// an enum type.
+struct Internal {
+    label:  String,
+    info:   Info,
+    child:  [Option<Arc<Node>>; 2],
+}
+
+// Flags are used to mark nodes that will be changed, instead of 
+// needing to use locks.
 enum Info {
     Flag {
-        test:   Arc<Node>,
+        flag:   Vec<Arc<Internal>>, // nodes to be flagged [0,4]
+        oldI:   Vec<Arc<Info>>,     // expected CAS value for flagging
+        unflag: Vec<Arc<Internal>>, // nodes to unflag [0,2]
+        par:    Vec<Arc<Internal>>, // nodes whose child changed [0,2]
+        old:    Vec<Arc<Node>>,     // expected children
+        new:    Vec<Arc<Node>>,     // new children
+        mvLeaf: Option<Arc<Leaf>>,  // leaf that may be flagged [0,1]
+        flagDone: bool,
     },
 
     Unflag,
 }
 
-
 impl CPT {
     fn new() -> CPT {
         CPT {
-            root: Arc::new(Leaf{label: String::new(), info: Info::Unflag}),
+            root: Arc::new(
+                Internal{ label: String::new(), 
+                          info:  Info::Unflag,
+                          child: [None, None], }
+                          ),
         }
     }
 }
