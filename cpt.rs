5288329c35834f484bc5eaa7969271cf41805a92
Kevin Lang <klang2012@gmail.com>
2015-03-21 20:41:58 -0400

    continuing to flesh out the data types the pt will use, added .gitignore

--- cpt.rs
+++ cpt.rs
@@ -6,4 +6,41 @@
 // http://www.temple.edu/cis/icdcs2013/data/5000a216.pdf
 // by Niloufar Shafiei
 
+use std::sync::Arc;
+use self::Node::*;
 
+pub struct CPT {
+    root: Arc<Node>
+}
+
+enum Node {
+    Leaf { 
+        label:  String,
+        info:   Info,
+    },
+
+    Internal {
+        label:  String,
+    }
+}
+
+enum Info {
+    Flag {
+        test:   Arc<Node>,
+    },
+
+    Unflag,
+}
+
+
+impl CPT {
+    fn new() -> CPT {
+        CPT {
+            root: Arc::new(Leaf{label: String::new(), info: Info::Unflag}),
+        }
+    }
+}
+
+fn main() {
+
+}
