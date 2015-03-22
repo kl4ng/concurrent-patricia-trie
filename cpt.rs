36f2f237133fe5a6748b8d4e8587464a3a1a0e7f
Kevin Lang <klang2012@gmail.com>
2015-03-22 18:50:36 -0400

    added Result type, fleshed out search but its by no means close to completion, still reasoning about how best to structure this in a java-like (like the pseudocode in the paper, presumably) manner, since Rust OOP is quite different

--- cpt.rs
+++ cpt.rs
@@ -7,10 +7,11 @@
 // by Niloufar Shafiei
 
 use std::sync::Arc;
+use std::option::Option;
 
 // The CPT simply is a root to the tree
 pub struct CPT {
-    root: Arc<Internal>
+    root: Arc<Node>,
 }
 
 enum Node {
@@ -34,15 +35,16 @@ struct Internal {
     child:  [Option<Arc<Node>>; 2],
 }
 
-// Flags are used to mark nodes that will be changed, instead of 
-// needing to use locks.
+// Info can either be a Flag, to designate some sort of change,
+// or, if nothing is happening, an Unflag
 enum Info {
     Flag(Flag),
     Unflag,
 }
 
+// These are used to facilitate insertion/deletion/etc
 struct Flag {
-    flag:   [Option<Arc<Internal>>; 4],// nodes to be flagged
+    flag:   [Option<Arc<Internal>>; 4], // nodes to be flagged
     oldI:   [Option<Arc<Info>>; 4],     // expected CAS value for flagging
     unflag: [Option<Arc<Internal>>; 2], // nodes to unflag
     par:    [Option<Arc<Internal>>; 2], // nodes whose child changed
@@ -52,14 +54,57 @@ struct Flag {
     flagDone: bool,
 }
 
+// This is the type returned by search(), which is leveraged by
+// most of the methods of the CPT.
+// gp = grandparent, p = parent
+struct Result {
+    gp:     Option<Arc<Internal>>,  // gp of node, if found
+    p:      Option<Arc<Internal>>,  // p of node, if found
+    node:   Node,                   // node we may have found
+    gpI:    Option<Arc<Node>>,      // expected children of gp
+    pI:     Option<Arc<Node>>,      // expected children of p
+    keyInTrie: bool,                // true if we found node
+}
+
 impl CPT {
     fn new() -> CPT {
         CPT {
-            root: Arc::new(
-                Internal{ label: String::new(), 
-                          info:  Info::Unflag,
-                          child: [None, None], }
-                          ),
+            root: Arc::new(Node::Internal(Internal{ 
+                label: String::new(), 
+                info:  Info::Unflag,
+                child: [None, None],
+            })),
+        }
+    }
+
+    // This is leveraged for most of the other algorithms, will return
+    // <gp, p, node, gpi, pi, keyInTrie>
+    fn search(&self, v: String) {
+        // p is Internal we may need to flag, pI are their children
+        let mut p: Option<Arc<Internal>> = None;
+        let mut pI:Option<Arc<Node>> = None; 
+
+        // likewise with grandparents
+        let mut gp: Option<Arc<Internal>> = None;
+        let mut gpI:Option<Arc<Node>> = None;
+
+        // Node we are looking for
+        let mut node = self.root.clone();
+        loop {
+            match *node {
+                Node::Internal(ref i) => {
+                    
+                },
+                _ => break,
+            }
+        }
+        
+    }
+
+    fn logicallyRemoved(I: Info) -> bool {
+        match I {
+            Info::Unflag  => false,
+            Info::Flag(f) => true, //TODO
         }
     }
 }
