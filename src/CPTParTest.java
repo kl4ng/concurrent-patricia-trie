b2c4a62dc9a92998dba3770fb7d7f743ef4bc637
Kevin Lang <klang2012@gmail.com>
2015-04-13 22:20:26 -0400

    completed test function, ran tests, added test data for cpt

--- src/CPTParTest.java
+++ src/CPTParTest.java
@@ -16,14 +16,14 @@ public class CPTParTest implements Runnable
     int threadID;
     public static int NUM_THREADS;
     public static int getPercent, insPercent, delPercent;
-    public static int iterations, keyRange;
+    public static int iterations, keyRange, initSize;
     
     // all threads have a static reference to a shared cpt object
     static ConcurrentPatriciaTrie<Integer> cpt;
     
     // info for how the tests actually perform
     static long timeSpent[];
-    static double Mops; // million of operations per second
+    static double ops;
   
     
     public CPTParTest(int id)
@@ -41,16 +41,16 @@ public class CPTParTest implements Runnable
         {
             int op = r.nextInt(100);
             
-            if(op < getPercent)    // 0-GP
+            if(op < getPercent)    // [0, GP)
             {
                 cpt.get(r.nextInt(keyRange)+1);
             }
-            else if(op < getPercent + insPercent)
+            else if(op < getPercent + insPercent)   // [GP, GP+IP)
             {
                 // doesn't matter what we insert
                 cpt.insert(r.nextInt(keyRange)+1, 1);
             }
-            else
+            else    // [GP+IP, 100]
             {
                 cpt.delete(r.nextInt(keyRange)+1);
             }
@@ -61,31 +61,55 @@ public class CPTParTest implements Runnable
     
     public static void main(String[] args) throws InterruptedException
     {
-        // set up testing environment vars
-        NUM_THREADS = 4;
-        getPercent = 60;
-        insPercent = 20;
-        delPercent = 20;
-        iterations = 10000000;
-        keyRange = 1000;
-        timeSpent = new long[NUM_THREADS];
-        cpt = new ConcurrentPatriciaTrie<Integer>();
-        
-        // create and start threads
-        Thread[] thread = new Thread[NUM_THREADS];
-        for(int i = 0; i < thread.length; i++)
+        // loop through all thread numbers
+        for(NUM_THREADS = 1; NUM_THREADS <= 16; NUM_THREADS *= 2)
         {
-            thread[i] = new Thread(new CPTParTest(i));
-            thread[i].start();
+            // set up testing environment vars
+            getPercent = 90;
+            insPercent = 9;
+            delPercent = 1;
+            iterations = 5000000 / NUM_THREADS;   // how many iterations each thread does
+            keyRange = 1000000;
+            initSize = 0;
+            timeSpent = new long[NUM_THREADS];
+            cpt = new ConcurrentPatriciaTrie<Integer>();
+            
+            // prepopulate
+            Random r = new Random();
+            for(int i = 0; i < initSize; i++)
+            {
+                cpt.insert(r.nextInt(keyRange)+1, 1);
+            }
+            
+            // create and start threads
+            Thread[] thread = new Thread[NUM_THREADS];
+            for(int i = 0; i < thread.length; i++)
+            {
+                thread[i] = new Thread(new CPTParTest(i));
+                thread[i].start();
+            }
+            
+            // wait till threads finish
+            for(int i = 0; i < thread.length; i++)
+            {
+                thread[i].join();   // block until thread finishes
+            }
+            
+            // collect and compute data here
+            long totalTimeSpent = 0;
+            for(long time : timeSpent)
+            {
+                totalTimeSpent += time;
+            }
+            double avgTime = (totalTimeSpent / 1000.0d) / (NUM_THREADS);
+            ops = (NUM_THREADS * iterations) / avgTime;
+            
+            System.out.printf("get/ins/del: %d / %d / %d\n", getPercent, insPercent, delPercent);
+            System.out.printf("Key range: %d\n", keyRange);
+            System.out.printf("Num threads: %d\n", NUM_THREADS);
+            System.out.printf("Throughput (op/sec): %f\n", ops);
+            System.out.println();
         }
-        
-        // wait till threads finish
-        for(int i = 0; i < thread.length; i++)
-        {
-            thread[i].join();   // block until thread finishes
-        }
-        
-        // collect and compute data here
     }
 
 }
